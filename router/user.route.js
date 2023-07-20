8019aad7a6a744f17b1a20d98d43cb2cde97cd72
rohit sahu <sahu86744@gmail.com>
2023-07-20 22:41:14 +0530

    d

--- router/user.route.js
+++ router/user.route.js
@@ -6,8 +6,6 @@ const contactModel = require("../models/contact.model");
 const nodemailer = require('nodemailer');
 const AWS = require("aws-sdk");
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
     region: process.env.region
 });
 
