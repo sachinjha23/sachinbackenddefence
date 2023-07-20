8191208575256120c85ea24f01e79f6886fe9d76
rohit sahu <sahu86744@gmail.com>
2023-07-20 10:19:46 +0530

    update aws ses

--- router/user.route.js
+++ router/user.route.js
@@ -4,6 +4,8 @@ const userrouter = router();
 const Course = require("../models/course.model");
 const contactModel = require("../models/contact.model");
 const nodemailer = require('nodemailer');
+const AWS = require("aws-sdk");
+AWS.config.update({ region: process.env.region });
 
 userrouter.get("/getcourse", async (req, res) => {
     try {
@@ -29,30 +31,30 @@ userrouter.get("/getMaterial", async (req, res) => {
 
 
 // Create a new contact
-userrouter.post('/contacts', (req, res) => {
-    const { name, email, phoneNumber, course } = req.body;
+// userrouter.post('/contacts', (req, res) => {
+//     const { name, email, phoneNumber, course } = req.body;
 
-    // Create a new contact document
-    const contact = new contactModel({
-        name,
-        email,
-        phoneNumber,
-        course,
-    });
+//     // Create a new contact document
+//     const contact = new contactModel({
+//         name,
+//         email,
+//         phoneNumber,
+//         course,
+//     });
 
-    // Save the contact to the database
-    contact.save((err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Failed to save contact' });
-        } else {
+//     // Save the contact to the database
+//     contact.save((err) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).json({ error: 'Failed to save contact' });
+//         } else {
 
-            sendEmailNotification(name, email, phoneNumber, course);
+//             sendEmailNotification(name, email, phoneNumber, course);
 
-            res.status(201).json({ message: 'Contact created successfully' });
-        }
-    });
-});
+//             res.status(201).json({ message: 'Contact created successfully' });
+//         }
+//     });
+// });
 
 // Get all contacts
 userrouter.get('/contacts', (req, res) => {
@@ -66,44 +68,68 @@ userrouter.get('/contacts', (req, res) => {
     });
 });
 
-// Send email notification
+// Send email notification using AWS SES
 function sendEmailNotification(name, email, phoneNumber, course) {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_FROM,
-            pass: process.env.EMAIL_FROM_PASSWORD,
-        },
-    });
+    const ses = new AWS.SES({ apiVersion: "2010-12-01" });
 
-    const message = {
-        from: email,
-        to: process.env.EMAIL_FROM,
-        subject: 'New Contact Form Submission',
-        html: `
-      <h3>New Contact Form Submission</h3>
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Phone Number: ${phoneNumber}</p>
-      <p>Course: ${course}</p>
-    `,
+    const params = {
+        Destination: {
+            ToAddresses: [process.env.EMAIL_FROM], // Replace with the recipient email address
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: `
+                        <h3>New Contact Form Submission</h3>
+                        <p>Name: ${name}</p>
+                        <p>Email: ${email}</p>
+                        <p>Phone Number: ${phoneNumber}</p>
+                        <p>Course: ${course}</p>
+                    `,
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "New Contact Form Submission",
+            },
+        },
+        Source: process.env.EMAIL_FROM, // Replace with the email address from which you want to send the email
     };
 
-    // Send the email
-    new Promise((resolve, reject) => {
-        transporter.sendMail(message, (error, response) => {
-            if (error) {
-                console.error('Error sending email:', error);
-                reject(error);
-            } else {
-                console.log('Email sent:', response.response);
-                resolve("Email sent successfully");
-            }
+    return ses.sendEmail(params).promise()
+        .then(() => {
+            console.log('Email sent successfully');
+            return "Email sent successfully";
         })
-    });
+        .catch(error => {
+            console.error('Error sending email:', error);
+            throw error;
+        });
 }
+
+// Create a new contact
+userrouter.post('/contacts', async (req, res) => {
+    const { name, email, phoneNumber, course } = req.body;
+
+    // Create a new contact document
+    const contact = new contactModel({
+        name,
+        email,
+        phoneNumber,
+        course,
+    });
+
+    // Save the contact to the database
+    try {
+        await contact.save();
+        await sendEmailNotification(name, email, phoneNumber, course);
+        res.status(201).json({ message: 'Contact created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to save contact or send email' });
+    }
+});
 
 
 module.exports = {
